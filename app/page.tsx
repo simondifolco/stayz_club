af536a7f7970fa391abd6d82279aecb7ff407068
Simon Difolco <91595740+simondifolco@users.noreply.github.com>
2025-03-10 11:51:26 +0100

    initial commit starter simon

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ export default async function Home() {
   return (
     <>
       <main className="flex-1 flex flex-col gap-6 px-4">
-      Hello Wolrd
+      Hello Worldjzjzbjz
       </main>
     </>
   );
