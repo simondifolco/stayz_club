9f2c7d343a4da8b2bef144b52e60d78262f974c3
Simon Difolco <91595740+simondifolco@users.noreply.github.com>
2025-03-10 11:48:18 +0100

    initial commit

--- app/protected/page.tsx
+++ app/protected/page.tsx
@@ -29,10 +29,6 @@ export default async function ProtectedPage() {
           {JSON.stringify(user, null, 2)}
         </pre>
       </div>
-      <div>
-        <h2 className="font-bold text-2xl mb-4">Next steps</h2>
-        <FetchDataSteps />
-      </div>
     </div>
   );
 }
--- app/protected/settings/page.tsx
+++ app/protected/settings/page.tsx
@@ -1,8 +1,8 @@
-export default async function Home() {
+export default function UserSettings() {
   return (
     <>
       <main className="flex-1 flex flex-col gap-6 px-4">
-      Hello Wolrd
+      Hello User Settings
       </main>
     </>
   );
